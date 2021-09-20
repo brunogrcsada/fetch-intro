bad632bee3cc6c2d7789f856bc958da1a74f77b4
Bruno Silva <bruno.silva@ada.ac.uk>
2021-09-20 12:30:13 +0100

    Make the website work

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Select the element with the id (submit-button)
 const submitButton = document.getElementById("submit-button");
 console.log(submitButton);
 
@@ -10,9 +11,12 @@ if(!header) {
 }
 
 const displayResults = function(res) {
+    clearContents(sectionResults);
+
     let dogFrame = document.createElement("div");
     let dogPic = document.createElement("img");
     dogFrame.classList.add("image__container");
+    console.log(res);
     if (res.status === "error") {
         sectionResults.appendChild(dogFrame).innerHTML = 
         '<h1>Something went wrong <i class="fas fa-paw"></i></h1>'
@@ -21,12 +25,19 @@ const displayResults = function(res) {
         dogPic.src = res.message;
         dogPic.alt = 'picture of the dog';
     }
+
+    headerShrink();
 };
 
+// Creates a function and assigns it to the variable headerShrink
 const headerShrink = function() {
     const headerTitle = document.querySelector(".header__title");
     const headerDes = document.querySelector(".header__description");
     const form = document.getElementById("form");
+
+    console.log(header.classList);
+
+    //Check if the header contains a class with "header" in it
     if (header.classList.contains("header")) {
         header.classList.remove("header");
         header.classList.add("header--small");
@@ -44,5 +55,14 @@ const clearContents = function(container) {
     }
 };
 
-// submitButton.addEventListener("click", headerShrink);
+const onFetchDog = e => {
+    e.preventDefault();
+    
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(res => displayResults(res));
+        //.then(res => displayResults(res.json()))
+}
+
+submitButton.addEventListener("click", onFetchDog);
 
